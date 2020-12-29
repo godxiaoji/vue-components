7a28183ff3ca2f33cc97d9bbea3ed063db313c04
DESKTOP-4SPP55M\Travis <godxiaoji@vip.qq.com>
2020-12-29 23:38:53 +0800

    build: use @babel/plugin-transform-runtime

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,13 @@
 module.exports = {
-  presets: [['@babel/preset-env', { modules: false }]],
+  presets: [['@babel/preset-env']],
+  plugins: [
+    [
+      '@babel/plugin-transform-runtime',
+      {
+        corejs: 3 // 指定 runtime-corejs 的版本，目前有 2 3 两个版本
+      }
+    ]
+  ],
   env: {
     test: {
       presets: [['@babel/preset-env', { targets: { node: 'current' } }]],
